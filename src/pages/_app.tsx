261c1f2111ca2b6922588a5fa2b5c80c2cddcac7
herbertdesousa <sousaherbert138@gmail.com>
2022-12-03 18:01:24 -0300

    fix: tailwindcss in dev dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import 'tailwindcss/tailwind.css';
 import '../styles/main.css';
 
