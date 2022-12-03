00ca331a160431264b62a7721f2000221c575d47
herbertdesousa <sousaherbert138@gmail.com>
2022-12-03 17:53:38 -0300

    update: add pulsar multimidea experiencie

--- src/pages/api/experiencies.ts
+++ src/pages/api/experiencies.ts
@@ -5,7 +5,15 @@ const data = [
     id: 'id-001',
     title: 'Guia do Transporte',
     img: '/experiencies/guia-do-transporte.jpg',
+    link: 'https://www.guiadotransporte.com.br',
     date: '2019-2022',
+  },
+  {
+    id: 'id-002',
+    title: 'Pulsar Multimedia',
+    img: '/experiencies/pulsar-multimidea.png',
+    link: 'http://www.pulsarmultimedia.com.br',
+    date: '2022',
   },
 ];
 
--- src/pages/experiencies.tsx
+++ src/pages/experiencies.tsx
@@ -6,12 +6,15 @@ import { Section } from '@/modules';
 import axios from 'axios';
 
 import { useLanguage } from '@/hooks/useLanguage';
+import Link from 'next/link';
+import { MdOpenInNew } from 'react-icons/md';
 
 interface IExperienciesProps {
   experiencies: {
     id: string;
     title: string;
     date: string;
+    link: string;
     img: string;
   }[];
 }
@@ -28,19 +31,33 @@ const Experiencies: React.FC<IExperienciesProps> = ({ experiencies }) => {
       <ul className="mt-12">
         {experiencies.map(item => (
           <li key={item.id} className="-m-1">
-            <button type="button" className="flex bg-white w-full">
-              <Image
-                src={item.img}
-                width="56"
-                height="56"
-                className="rounded-full"
-              />
+            <div className="flex bg-white w-full">
+              <div className="border border-gray rounded-full h-14 w-14 p-1">
+                <Image
+                  src={item.img}
+                  width="56"
+                  height="56"
+                  objectFit="contain"
+                  className="rounded-full"
+                />
+              </div>
               <div className="ml-4">
-                <strong className="text-left">{item.title}</strong>
-                <br />
+                <strong className="text-left">
+                  <Link href={item.link} passHref>
+                    <a
+                      href={item.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex items-center"
+                    >
+                      {item.title}
+                      <MdOpenInNew size={16} className="ml-1 text-primary" />
+                    </a>
+                  </Link>
+                </strong>
                 <span className="flex items-center text-gray">{item.date}</span>
               </div>
-            </button>
+            </div>
             <div
               className="ml-7 h-16 border-l-2 border-dashed border-gray"
               style={{ width: 0 }}
